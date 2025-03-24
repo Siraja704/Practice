d7a433fee55bdd68599f6e3780531a21f348e05b
Siraja704 <janishab1704@gmail.com>
2025-03-24 23:48:16 +0500

    updated work

--- src/commonents/Counter.js
+++ src/commonents/Counter.js
@@ -2,19 +2,15 @@ import react, { Component } from "react";
 
 class Counter extends Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {
-      count: 0,
+      counter: 0,
     };
   }
 
   increament() {
-    // this.setState({
-    //   count: this.state.count + 1,
-    // });
-
     this.setState((prevState) => ({
-      count: prevState.count + 1,
+      counter: prevState.counter + 1,
     }));
   }
 
@@ -25,26 +21,75 @@ class Counter extends Component {
     this.increament();
     this.increament();
   }
+
   reset() {
     this.setState({
-      count: 0,
+      counter: 0,
     });
   }
 
   render() {
     return (
       <div>
-        <div>count = {this.state.count}</div>
-        <button onClick={() => this.increament()}>Increament</button>
-        <button onClick={() => this.increamentFive()}>
-          {" "}
-          increament of Five
-        </button>
-
-        <button onClick={() => this.reset()}>Reset button</button>
+        <div>Counter Machine</div>
+        <div>Count - {this.state.counter}</div>
+        <button onClick={() => this.increament()}>Increament by 1</button>
+        <button onClick={() => this.increamentFive()}>Increament by 5</button>
+        <button onClick={() => this.reset()}>reset</button>
       </div>
     );
   }
 }
 
 export default Counter;
+
+// import react, { Component } from "react";
+
+// class Counter extends Component {
+//   constructor(props) {
+//     super();
+//     this.state = {
+//       count: 0,
+//     };
+//   }
+
+//   increament() {
+//     // this.setState({
+//     //   count: this.state.count + 1,
+//     // });
+
+//     this.setState((prevState) => ({
+//       count: prevState.count + 1,
+//     }));
+//   }
+
+//   increamentFive() {
+//     this.increament();
+//     this.increament();
+//     this.increament();
+//     this.increament();
+//     this.increament();
+//   }
+//   reset() {
+//     this.setState({
+//       count: 0,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <div>count = {this.state.count}</div>
+//         <button onClick={() => this.increament()}>Increament</button>
+//         <button onClick={() => this.increamentFive()}>
+//           {" "}
+//           increament of Five
+//         </button>
+
+//         <button onClick={() => this.reset()}>Reset button</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
